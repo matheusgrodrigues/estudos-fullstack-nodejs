ac416650d0ef41ebd45feb128ef71edbbebdda1d
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-10-31 17:28:44 -0300

    Pulando etapa de testes

--- ntask-api/test/routes/index.ts
+++ ntask-api/test/routes/index.ts
@@ -1,3 +1,6 @@
 // TODO: Pulei o capitulo de testes porque o mocha não estava compilando e eu começei a ficar puto.
 // Um dia rever o livro e ir criando os testes para a aplicação.
-export default {};
+
+const test = {};
+
+export default test;
